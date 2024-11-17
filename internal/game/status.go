898623cd18d39eb80236f228b53ef6c4206cc2f6
Key Clark <aryeh.the.lion@gmail.com>
2024-11-16 22:57:29 -0600

    update status effects

--- internal/game/status.go
+++ internal/game/status.go
@@ -4,11 +4,11 @@ package game
 type StatusEffect string
 
 const (
-	StatusAccelerate StatusEffect = "ACCELERATE" // Each round increase speed by percentage based on potency.
-	StatusBurning    StatusEffect = "BURNING"    // Damage over time, damage increases by some formula that uses the duration each round.
-	StatusPoisoned   StatusEffect = "POISON"     // Damage over time, damage decreases by some formula that uses the duration each time.
-	StatusEnraged    StatusEffect = "ENRAGED"    // Each round increase Attack power by percentage based on potency.
-	StatusRecovering StatusEffect = "RECOVERING" // Each round a percentage of target health is recovered based on potency.
+	StatusAccelerate   StatusEffect = "ACCELERATE" // Each round increase speed by percentage based on potency.
+	StatusBurning      StatusEffect = "BURNING"    // Damage over time, damage increases by some formula that uses the duration each round.
+	StatusPoisoned     StatusEffect = "POISON"     // Damage over time, damage decreases by some formula that uses the duration each time.
+	StatusEnraged      StatusEffect = "ENRAGED"    // Each round increase Attack power by percentage based on potency.
+	StatusRegenerating StatusEffect = "REGENERATING"
 )
 
 // EVery effect will hold various attributes
